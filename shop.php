e65241ee824aaf83f009c04dce5b9508bec2a2e3
Keai <chenjy527@gmail.com>
2018-04-16 04:27:11 +0000

    all method

--- shop.php
+++ shop.php
@@ -4,12 +4,19 @@ class shop {
 	// show all items
 	public function allMethod() {
 		echo "all method";
+
 		$conn = new DBConnection();
+		$all = $conn->getAllItemsReturnObj();
+		echo $all;
 	}
 
 	// show item where id=3
 	public function itemMethod($id) {
 		echo "item method" . $id;
+
+		$id = $_POST['id'];
+		$conn = new DBConnection();
+		// $stmt = $conn->query("SELECT * FROM Item where ")
 	}
 	public function indexMethod() {
 		echo "Using Default Method";
